024b0d1e4ece6d71c8174de8cbc1f3af1646b240
Ponto Mega <contato@pontomega.com.br>
2016-05-22 03:16:08 -0300

    First Version

---
+++ mod_pm_article_image_spot.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @copyright	Copyright © 2016 - All rights reserved.
+ * @license		GNU General Public License v2.0
+ * @generator	http://xdsoft/joomla-module-generator/
+ */
+defined('_JEXEC') or die;
+
+$doc = JFactory::getDocument();
+/* Available fields:"image_in_the_spot","custom_image", */
+// Include assets
+$doc->addStyleSheet(JURI::root()."modules/mod_pm_article_image_spot/assets/css/style.css");
+$doc->addScript(JURI::root()."modules/mod_pm_article_image_spot/assets/js/script.js");
+// $width 			= $params->get("width");
+
+/**
+	$db = JFactory::getDBO();
+	$db->setQuery("SELECT * FROM #__mod_pm_article_image_spot where del=0 and module_id=".$module->id);
+	$objects = $db->loadAssocList();
+*/
+require JModuleHelper::getLayoutPath('mod_pm_article_image_spot', $params->get('layout', 'default'));
---
+++ tmpl/default.php
@@ -0,0 +1,28 @@
+<?php
+
+defined('_JEXEC') or die;
+
+
+$input=Jfactory::getApplication()->input;
+if($input->getCmd('option')=='com_content' 
+&& $input->getCmd('view')=='article' ){
+  $db=JFactory::getDbo(); 
+  $db->setQuery('select catid from #__content where id='.$input->getInt('id')); 
+  $catid=$db->loadResult(); 
+$db->setQuery('select images from #__content where id='.$input->getInt('id')); 
+  $images=$db->loadResult(); 
+}
+
+$theimage =(json_decode($images));
+$custom_image = $params->get('custom_image');
+echo '<div class="image_spot" style="height:'.$params->get('height').'>';
+if (($params->get('image_in_the_spot') == 'intro_image') and (!empty($theimage->image_intro))){
+echo  '<img src="'.$theimage->image_intro.'" />';
+}
+if (($params->get('image_in_the_spot') == 'full_image') and (!empty($theimage->image_fulltext))){
+echo  '<img src="'.$theimage->image_fulltext.'" />';
+}
+if (($params->get('image_in_the_spot') == 'custom_image') and (!empty($custom_image))){
+echo  '<img src="'.$params->get('custom_image').'" />';
+}
+echo '</div>';
